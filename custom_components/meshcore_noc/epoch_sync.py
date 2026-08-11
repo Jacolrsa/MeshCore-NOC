d0711293cb9ea7d572b053b84d4ec87c2870b917
Jacolrsa <jacolrsa@gmail.com>
2026-08-11 12:46:36 +0200

    chore: activate beta12 clock workflow

--- custom_components/meshcore_noc/epoch_sync.py
+++ custom_components/meshcore_noc/epoch_sync.py
@@ -1,5 +1,5 @@
 """Compatibility import for the current MeshCore NOC clock-sync workflow."""
 
-from .epoch_sync_beta11 import install_epoch_sync
+from .epoch_sync_beta12 import install_epoch_sync
 
 __all__ = ("install_epoch_sync",)
